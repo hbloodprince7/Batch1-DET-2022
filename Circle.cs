a0bdca4453988fe0b4f9477164d39c1256f6c511
hbloodprince7 <manvithpshetty@gmail.com>
2022-08-26 12:37:48 +0530

    Added Circle

---
+++ Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    public class Circle
+    {
+        int r;
+        public Circle(int radius)
+        {
+            this.r = radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * r;
+        }
+          
+    }
+
+}
+
