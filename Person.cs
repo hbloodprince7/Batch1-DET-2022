8e8b179e9e0ec3cb0e5ab03eb78214fcab309d4b
hbloodprince7 <manvithpshetty@gmail.com>
2022-08-29 10:05:42 +0530

    minute additons

--- Person.cs
+++ Person.cs
@@ -7,7 +7,30 @@ using System.Threading.Tasks;
 namespace Batch1_DET_2022
 {
     public class Person
-    {   
+    {
+        string FirstName;
+        string LastName;
+        string Email;
+        DateOnly DOB;
         
+        public Person(string firstName, string lastName, string email, DateOnly dOB)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.DOB = dOB;
+        }
+        public Person(string firstName,string lastName, string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+        }
+        public Person(string firstName, string lastName, DateOnly dOB)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.DOB = dOB;
+        }
     }
 }
