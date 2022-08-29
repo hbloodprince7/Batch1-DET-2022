9ee455129a1b09464c96064248a053774374e601
hbloodprince7 <manvithpshetty@gmail.com>
2022-08-29 14:43:20 +0530

    Age Exception added

---
+++ TestPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    public class TestPerson
+    {
+        public static void Main()
+        {
+            Person t = new Person("Tony", "Stark", "tonystark@stark.com", new DateOnly(19, 02, 1982));
+            string r = t.GetSunSign();
+        }
+    }
+}
