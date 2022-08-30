61adb2f7399937268968ac085a6f73fa06f65f7a
hbloodprince7 <manvithpshetty@gmail.com>
2022-08-30 17:03:59 +0530

    Added NewPerson and Human Classes

--- NewPerson.cs
+++ NewPerson.cs
@@ -8,6 +8,30 @@ namespace Batch1_DET_2022
 {
     public class NewPerson
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
 
+    public class NewStudent:NewPerson
+    {
+        public int YearStatus { get; set; }
+    }
+
+    public class Employee:NewPerson
+    {
+        public string Office { get; set; }
+        public int Salary { get; set; }
+        public DateOnly DateWhenHired { get; set; }
+    }
+
+    public class Faculty: Employee
+    {
+        public string Title { get; set; }
+    }
+
+    public class Staff: Employee
+    {
     }
 }
