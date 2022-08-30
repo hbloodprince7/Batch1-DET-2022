197de60889fee9d3b944ee21ec6176c7bd5aec3b
hbloodprince7 <manvithpshetty@gmail.com>
2022-08-30 11:46:15 +0530

    Delgates added

---
+++ DelImps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class DelImps
+    {
+        public static void Main()
+        {
+            Action<string> act = (string name) => { Console.WriteLine($"Hello Mr. {name}"); };
+            act.Invoke("Stark");
+
+            Action<int, string> act1 = (int s, string sender) => { Console.WriteLine($"There are {s} new Messages from {sender}"); };
+            act1.Invoke(4, "Ms. Potts");
+
+            Action<int, string, string> act2 = (int a, string b, string c) => { Console.WriteLine($"There have been {a} sightings of {b} around {c} Area"); };
+            act2.Invoke(7, "Bucky Barnes", "New York");
+        }
+    }
+}
