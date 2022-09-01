f62f929ff762162b2b7b6c93e11b372c3c0c248c
hbloodprince7 <manvithpshetty@gmail.com>
2022-09-01 10:19:49 +0530

    Added LinQ.cs

---
+++ LinQ.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class LinQ
+    {
+        public static void Main()
+        {
+            SampleInList();
+
+
+
+
+        }
+        private static void SampleInList()
+        {
+            //1.DATA SOURCE
+            List<string> avengers = new List<string>();
+            avengers.Add("Iron Man");
+            avengers.Add("Captain America");
+            avengers.Add("Hawk Eye");
+            avengers.Add("Hulk");
+
+
+            //2.QUERY CREATION
+            //DEFERRED EXECUTION:
+            //var numQuery = from num in numbers where num>100 select num;
+            var AvengersQuery = from hero in avengers select hero;
+            //EARLY EXECUTION:
+            //var numQuery = (from num in numbers select num).ToList();
+            avengers.Add("Thor Odinson");
+            avengers.Add("Black Widow");
+ 
+            //3.QUERY EXECUTION
+            foreach (string hero in AvengersQuery)
+            {
+                Console.WriteLine($"{hero}");
+            }
+        } 
+    }
+}
